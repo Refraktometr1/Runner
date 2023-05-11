4b3d2393b83bb30aa20d411f650cfd22b372cce3
Refraktometr1 <filatovsj@gmail.com>
2023-05-11 18:39:48 +0300

    add curve world shader

---
+++ Runner/Assets/Codebase/CameraLogic/WorldCurver.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class WorldCurver : MonoBehaviour
+{
+	[Range(-0.01f, 0.01f)]
+	public float curveVerticalStrength = 0.001f;
+
+    int m_CurveStrengthID;
+
+    private void OnEnable()
+    {
+        m_CurveStrengthID = Shader.PropertyToID("_CurveStrength");
+    }
+
+	void Update()
+	{
+		Shader.SetGlobalFloat(m_CurveStrengthID, curveVerticalStrength);
+	}
+}
